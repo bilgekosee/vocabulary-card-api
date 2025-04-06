4db4401bfa779655adbb9387873ecb84e83f42d9
bilgekosee <bilge.koosee@gmail.com>
2025-04-06 23:57:29 +0300

    update

--- src/sidebar/sidebar.jsx
+++ src/sidebar/sidebar.jsx
@@ -1,7 +1,16 @@
 import "./sidebar.css";
 
 const Header = () => {
-  return <div className="sidebar-card"></div>;
+  return (
+    <div className="sidebar-card">
+      <div className="name-site">
+        <span>WISE VOCABULARY</span>
+      </div>
+      <div className="card-type-sidebar">
+        <span className="card-title">CARDS</span>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
