7d2abcd0fa551f8aa498ed884024b41ae10a4cdb
bilgekosee <bilge.koosee@gmail.com>
2025-04-08 14:41:12 +0300

    cards and reading section added

--- src/sidebar/sidebar.jsx
+++ src/sidebar/sidebar.jsx
@@ -1,13 +1,79 @@
+import { useState } from "react";
 import "./sidebar.css";
+import { TbCardsFilled } from "react-icons/tb";
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
+import { GiRead } from "react-icons/gi";
 
 const Header = () => {
+  const [isOpen, setİsOpen] = useState(true);
+  const [open, setOpen] = useState(true);
+  const [readinOpen, setReadingOpen] = useState(true);
+
+  const toggleOpen = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+  const toggleOpenRead = () => {
+    setReadingOpen((prevOpen) => !prevOpen);
+  };
+
   return (
-    <div className="sidebar-card">
-      <div className="name-site">
-        <span>WISE VOCABULARY</span>
+    <div className={`sidebar-card ${isOpen ? "open" : "close"}`}>
+      <div
+        className="name-site"
+        onClick={() => {
+          setİsOpen(!isOpen);
+        }}
+      >
+        {isOpen ? (
+          <span className="site-name-title">WISE VOCABULARY</span>
+        ) : (
+          <span className="site-name-title">WV</span>
+        )}
       </div>
-      <div className="card-type-sidebar">
-        <span className="card-title">CARDS</span>
+
+      <div className="tab">
+        <div className="type-cards-wrapper" onClick={toggleOpen}>
+          <div className="cardsand-icon">
+            <TbCardsFilled className="cards-icon" />
+            <span className="card-title">CARDS</span>
+          </div>
+          {open ? (
+            <IoIosArrowDown className="cards-icon" />
+          ) : (
+            <IoIosArrowUp className="cards-icon" />
+          )}
+        </div>
+
+        {open && (
+          <ul className="submenu">
+            <li>Card Type 1</li>
+            <li>Card Type 2</li>
+            <li>Card Type 3</li>
+            <li>Card Type 4</li>
+          </ul>
+        )}
+      </div>
+      <div className="tab">
+        <div className="type-cards-wrapper" onClick={toggleOpenRead}>
+          <div className="cardsand-icon">
+            <GiRead className="cards-icon" />
+            <span className="card-title">READING</span>
+          </div>
+          {readinOpen ? (
+            <IoIosArrowDown className="cards-icon" />
+          ) : (
+            <IoIosArrowUp className="cards-icon" />
+          )}
+        </div>
+
+        {readinOpen && (
+          <ul className="submenu">
+            <li>Read Type 1</li>
+            <li>Read Type 2</li>
+            <li>Read Type 3</li>
+            <li>Read Type 4</li>
+          </ul>
+        )}
       </div>
     </div>
   );
