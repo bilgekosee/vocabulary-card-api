faf454a47eccbc94721920767d2817779cb89142
bilgekosee <bilge.koosee@gmail.com>
2025-04-14 22:01:23 +0300

    card 4 data added

--- src/cards/VocabularyType4/VocabularyType4.jsx
+++ src/cards/VocabularyType4/VocabularyType4.jsx
@@ -1,28 +1,49 @@
 import "./VocabularyType4.css";
+import { useState } from "react";
 import { MdTranslate } from "react-icons/md";
 import {
   PiArrowCircleRightDuotone,
   PiArrowCircleLeftDuotone,
 } from "react-icons/pi";
+import Vocabulary from "../../data/vocabulary_complete.json";
 
 const VocabularyCardFourth = () => {
+  const [indexCardFour, setIndexCardFour] = useState(0);
+  const [flipCard, setFlipCard] = useState(false);
+  const currentWord = Vocabulary[indexCardFour];
+
+  const nextCardFour = () => {
+    setIndexCardFour((prevIndex) =>
+      prevIndex < Vocabulary.length - 1 ? prevIndex + 1 : 0
+    );
+    setFlipCard(false);
+  };
+  const prevCardFour = () => {
+    setIndexCardFour((prevIndex) =>
+      prevIndex > 0 ? prevIndex - 1 : Vocabulary.length - 1
+    );
+    setFlipCard(false);
+  };
   return (
     <div className="container-card-fourth">
       <div className="card-fourth">
-        <h1>Solace</h1>
+        <h1>{currentWord.english}</h1>
         <span className="card-fourth-info">
           <MdTranslate />
         </span>
         <div className="card-fourth-answer">
-          <p>
-            Üzüntü veya sıkıntı anında içini biraz olsun rahatlatan şey, huzur,
-            teselli
-          </p>
+          <p>{currentWord.turkish}</p>
         </div>
       </div>
       <div className="card-fourth-arrowsicon-wrapper">
-        <PiArrowCircleLeftDuotone className="card-fourth-arrowsicon left" />
-        <PiArrowCircleRightDuotone className="card-fourth-arrowsicon right" />
+        <PiArrowCircleLeftDuotone
+          className="card-fourth-arrowsicon left"
+          onClick={prevCardFour}
+        />
+        <PiArrowCircleRightDuotone
+          className="card-fourth-arrowsicon right"
+          onClick={nextCardFour}
+        />
       </div>
     </div>
   );
