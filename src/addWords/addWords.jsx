05ae8bca2bef70889461e0658c084e727f11284d
bilgekosee <bilge.koosee@gmail.com>
2025-04-22 13:45:37 +0300

    addwords alanı eklendi

--- src/addWords/addWords.jsx
+++ src/addWords/addWords.jsx
@@ -2,6 +2,16 @@ import "./addWords.css";
 import { useNavigate } from "react-router-dom";
 const AddWords = () => {
   const navigate = useNavigate();
-  return <div>bilge</div>;
+  return (
+    <div className="addword-container">
+      <div className="add-english-turkish">
+        <from className="addword-wrapper">
+          <input type="text" placeholder="English word" required></input>
+          <input type="text" placeholder="Turkish word" required></input>
+          <button type="submit">Add</button>
+        </from>
+      </div>
+    </div>
+  );
 };
 export default AddWords;
